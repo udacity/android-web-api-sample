d9d0982be74141efea3c0b81e50afd83dc00a5b1
Daniel Mai <daniel@udacity.com>
2015-07-02 13:56:04 -0700

    feat: Add Spotify wrapper call in AsyncTask.

--- app/src/main/java/demo/example/com/spotifysample/MainActivity.java
+++ app/src/main/java/demo/example/com/spotifysample/MainActivity.java
@@ -1,17 +1,31 @@
 package demo.example.com.spotifysample;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SearchSpotifyTask task = new SearchSpotifyTask();
+        task.execute();
     }
 
     @Override
@@ -34,5 +48,22 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class SearchSpotifyTask extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... strings) {
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService service = api.getService();
+
+            ArtistsPager results = service.searchArtists("Paul");
+            List<Artist> artists = results.artists.items;
+            for (int i = 0; i < artists.size(); i++) {
+                Artist artist = artists.get(i);
+                Log.i(LOG_TAG, i + " " + artist.name);
+            }
+            return null;
+        }
     }
 }
